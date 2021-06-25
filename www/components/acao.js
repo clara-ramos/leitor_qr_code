3be8f3bd461de7356e810e8ba28d9506aa8ddd53
clara-ramos <mariaclaraaraujoramos22@gmail.com>
2021-06-25 11:23:24 +0900

    Project's Initial Commit

---
+++ www/components/acao.js
@@ -0,0 +1,27 @@
+$(document).on("click", "#codigo", function(){
+  cordova.plugins.barcodeScanner.scan(
+    function (result) {
+      var cancelado = result.cancelled;
+      if(cancelado === false){
+        var resultado = $("#result");
+        resultado.text(result.text);
+      }
+    },
+    function (error) {
+      alert("O escaneamento falhou: " + error);
+    },
+    {
+      preferFrontCamera : false,
+      showFlipCameraButton : true,
+      showTorchButton : true,
+      torchOn: false,
+      saveHistory: true,
+      prompt : "Aponte a um Código de barras ou QR Code",
+      resultDisplayDuration: 500,
+      formats : "default",
+      orientation : "default",
+      disableAnimations : true,
+      disableSuccessBeep: false 
+    }
+  );
+});
